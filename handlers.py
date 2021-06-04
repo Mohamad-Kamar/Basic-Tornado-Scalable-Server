43ad5d705e15ccc0037107bf7436961869b8217d
Mohamad-Kamar <mohamad.kamar@saugo360.com>
2021-06-04 14:04:39 +0300

    Add .json to print response body

--- handlers.py
+++ handlers.py
@@ -3,6 +3,7 @@ import tornado.web
 from publisher import execute
 import requests
 class basicRequestHandler(tornado.web.RequestHandler):
+    SUPPORTED_METHODS = ("GET")
     def get(self):
         self.write("Hello, world!!!!!!")
 
@@ -13,7 +14,7 @@ class pikaRequestHandler(tornado.web.RequestHandler):
 class elasticRequestHandler(tornado.web.RequestHandler):
     def get(self, id):
         response =requests.get(f"http://localhost:5601/controller/_doc/{id}")
-        self.write(response)
+        self.write(response.json())
 
 class queryStringRequestHandler(tornado.web.RequestHandler):
     def get(self):
